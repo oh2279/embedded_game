603e5346108ca10f5015a2fbaa7f23f8009e01d1
oh2279 <85153493+oh2279@users.noreply.github.com>
2021-06-24 00:20:42 +0900

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,10 +1,29 @@
 import filght
 from bullet import bullet 
+from PIL import Image, ImageDraw, ImageFont, ImageChops
+import random
+import time
 
+class enemy():
+        image = Image.open("/home/pi/beom/embedded_game/enemy.png")
+        def __init__(self):
+                self.spot=[]
+                self.x1=self.x2=self.x3=self.x4 = 0
+                self.y = 0
+                
+        def  regeneration(self):
+                self.x1 = random.randint(0,40)
+                self.x2 = random.randint(60,100)
+                self.x3 = random.randint(120,160)
+                self.x4= random.randint(180,220)
+                self.spot.append([self.x1,self.x2,self.x3,self.x4,self.y])
+                
+       # def move(self):
+                
 
-
-def fire(self,x,y): # x,y는 드래곤의 좌표
-        return bullet(2,self.x,self.y,x,y) # 2=적
+        def fire(self,x,y): # x,y는 드래곤의 좌표
+                shot = bullet(x,y)
+                shot.auto_fire()
 
 # 아래로 수직 하강
 
