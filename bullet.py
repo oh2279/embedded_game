ba8b72e5391e63e60a8ce7eeb64dcf906766dd3a
oh2279 <85153493+oh2279@users.noreply.github.com>
2021-06-24 00:21:30 +0900

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,24 +1,18 @@
-from os import X_OK
-
-
+from PIL import Image, ImageDraw, ImageFont
+import time
 class bullet():
-    def __init__(self,owner, x,y,ex,ey):#현재 좌표(출발 위치, 도착위치))
-    # 아군(위로 직진), 적군(드래곤을 향해), 방향 ,현재위치
-        self.owner = owner
-        self.x = x
-        self.y = y
-        self.dx = ex-x
-        self.dy = ey-y
-        d = math.sqrt(self.dx**2 + self.dy**2) # 속도 맞춰주기
-        self.dx /= d
-        self.dy /= d
-
-    def move(self):
-        self.x += dx
-        self.y += dy
-
-    def check(x, y, sx, sy, ex, ey):
-        sx <= x <= ex and sy <= y <= ey
+    image = Image.open("/home/pi/beom/embedded_game/bullet.png")
+    def __init__(self,current_x,current_y):
+        self.current_x = current_x
+        self.current_y = current_y
+        self.shots = []
+    def fire(self,dragon_x,dragon_y):
+        self.shots.append([dragon_x + 10,dragon_y])
+    
+    def auto_fire(self):
+        self.image.paste(self.image,(self.current_x,self.current_y))
+    def check(self):
+        trash =1
 
 
 # 히트박스(사각형), 총알의 왼쪽 위 좌표, 오른쪽 아래 좌표, 용의 왼쪽위 용의 오른쪽 아래 - 총 4개
@@ -30,7 +24,7 @@ class bullet():
 # sx1, sy1, ex1, ey1
 # sx2, sy2, ex2, ey2
 
-check(sx1, sy1, sx2, sy2, ex2, ey2) or check(sx1, ey1, sx2, sy2, ex2, ey2) or
-check(ex1, sy1, sx2, sy2, ex2, ey2) or check(ex1, ey1, sx2, sy2, ex2, ey2)  # 총알이 용과 겹쳤는지 판단
+#check(sx1, sy1, sx2, sy2, ex2, ey2) or check(sx1, ey1, sx2, sy2, ex2, ey2) or
+#check(ex1, sy1, sx2, sy2, ex2, ey2) or check(ex1, ey1, sx2, sy2, ex2, ey2)  # 총알이 용과 겹쳤는지 판단
 
 # 총알이 1, 용이 2
