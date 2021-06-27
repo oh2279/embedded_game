1bdc0cdf395c7641e3d628ed4c556e66b0641e84
oh2279 <85153493+oh2279@users.noreply.github.com>
2021-06-28 00:01:56 +0900

    Update dragon.py

--- dragon.py
+++ dragon.py
@@ -1,45 +1,52 @@
-#import filght
-from bullet import bullet 
-from PIL import Image, ImageDraw, ImageFont, ImageChops
+
+from PIL import Image
 
 
 class character():#filght.filght):
-    image = Image.open("/home/pi/beom/embedded_game/character.png")
+    image = Image.open("/home/pi/beom/embedded_game/img/character.png")
+    life = 3
+    move_speed = 7
+    more_speed = 0
+    dy = [-1,0,1,0] # 위, 오른쪽, 아래, 왼쪽
+    dx = [0,1,0,-1]
     def __init__(self,current_x,current_y):
         self.current_x = current_x
         self.current_y = current_y
 
+    def move(self,dir):
+        nx = self.current_x + self.dx[dir] * (self.move_speed + self.more_speed)
+        ny = self.current_y + self.dy[dir] * (self.move_speed + self.more_speed)
+        if nx < 5 or nx > 205 or ny < 5 or ny > 205 :
+            return
+        self.current_x = nx
+        self.current_y = ny
+
+        
     #좌로이동
-    def move_left(self):
-        if self.current_x < 5:
-            self.current_x = self.current_x
-        else:
-            self.current_x -= 5
-        self.image.paste(self.image,(self.current_x,self.current_y))
+    #def move_left(self):
+        #if self.current_x < 5:
+         #   self.current_x = self.current_x
+        #else:
+            #self.current_x = self.current_x - self.move_speed - self.more_speed
 
-    #우로이동
-    def move_right(self):
-        if self.current_x > 205:
-            self.current_x = self.current_x
-        else:
-            self.current_x += 5
-        self.image.paste(self.image,(self.current_x,self.current_y))
+    def check_hp(self,monster):
+         if len(monster) > 0:
+            for j in range(len(monster))[::-1]:
+                for k in range(4):
+                    if self.current_x + 30 > monster[j][k] and monster[j][k] + 30 > self.current_x and self.current_y <= monster[j][4] +30:
+                        if self.life >0:
+                            self.life -= 1
+                        monster[j][k] = -3000
+                        #if self.life == 0:
+                            #self.current_x = -240
+                            #self.current_y = 240
 
-    #위로이동
-    def move_up(self):
-        if self.current_y < 5:
-            self.current_y = self.current_y
-        else:
-            self.current_y -= 5
-        self.image.paste(self.image,(self.current_x,self.current_y))
+    def check_hp_to_boss(self,boss):
+         if len(boss) > 0:
+            if self.current_x + 30 > boss[0][0] and boss[0][0] + 50 > self.current_x and self.current_y <= boss[0][1] +50 :
+                self.life = 0
 
-        #아래로 이동
-    def move_down(self):
-        if self.current_y > 205:
-            self.current_y = self.current_y
-        else:
-            self.current_y += 5
-        self.image.paste(self.image,(self.current_x,self.current_y))
-
-
+    def get_life(self):
+        return self.life
+                            
 
