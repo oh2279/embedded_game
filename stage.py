49686e544cb50d8e896cf1a863ef259dbefdfd68
oh2279 <85153493+oh2279@users.noreply.github.com>
2021-06-28 00:00:21 +0900

    Update stage.py

--- stage.py
+++ stage.py
@@ -1,3 +1,10 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 
-image = Image.open("/home/pi/beom/embedded_game/background.png")
+
+background = Image.open("/home/pi/beom/embedded_game/background.png")   
+ending = Image.open("/home/pi/beom/embedded_game/ending.png")
+life1 = Image.open("/home/pi/beom/embedded_game/life1.png")
+life2 = Image.open("/home/pi/beom/embedded_game/life2.png")
+life3 = Image.open("/home/pi/beom/embedded_game/life3.png")
+explosion = Image.open("/home/pi/beom/embedded_game/explosion.png")
+start = Image.open("/home/pi/beom/embedded_game/start.png")
