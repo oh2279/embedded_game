7c2395c6c59239235365d8223059a0a53dfe3bb8
oh2279 <85153493+oh2279@users.noreply.github.com>
2021-06-28 20:33:35 +0900

    Update stage.py

--- stage.py
+++ stage.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
-
-background = Image.open("/home/pi/beom/embedded_game/background.png")   
-ending = Image.open("/home/pi/beom/embedded_game/ending.png")
-life1 = Image.open("/home/pi/beom/embedded_game/life1.png")
-life2 = Image.open("/home/pi/beom/embedded_game/life2.png")
-life3 = Image.open("/home/pi/beom/embedded_game/life3.png")
-explosion = Image.open("/home/pi/beom/embedded_game/explosion.png")
-start = Image.open("/home/pi/beom/embedded_game/start.png")
+background = Image.open("/home/pi/beom/embedded_game/img/background.png")   
+ending = Image.open("/home/pi/beom/embedded_game/img/ending.png")
+life1 = Image.open("/home/pi/beom/embedded_game/img/life1.png")
+life2 = Image.open("/home/pi/beom/embedded_game/img/life2.png")
+life3 = Image.open("/home/pi/beom/embedded_game/img/life3.png")
+explosion = Image.open("/home/pi/beom/embedded_game/img/explosion.png")
+start = Image.open("/home/pi/beom/embedded_game/img/start.png")
+win = Image.open("/home/pi/beom/embedded_game/img/win.png")
